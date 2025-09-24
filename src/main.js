50b61ce845698b0a0c4eb59df9c08c68fb882ebd
ievgenii_prokopenko <ievgeniiprokopenko@gmail.com>
2025-09-24 18:29:48 +0200

    test

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// import suma from "./scripts/helper";
+
+// console.log(suma(300, 100));
+// // import { userName as myUser, sayHello } from "./scripts/helper";
+
+// import * as services from "./scripts/helper";
+// console.log(services.userName);
+// console.log(services.sayHello());
+
+
+
+// // const userName = "Galyna";
+
+// // console.log(userName);
+// // console.log(sayHello());
+// // console.log(myUser);
+
+
+import { nanoid } from 'nanoid'
+console.log(nanoid());
